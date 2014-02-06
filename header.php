74fa9f2fbcf0dc4608dc3ee31db473d96c7a4958
themitchuation <mitch@everyvote.org>
2014-02-05 21:27:02 -0600

    Initial push

---
+++ header.php
@@ -0,0 +1,32 @@
+<div class="col-sm-4 header-left">
+  <div class="header-nav">
+    <div class="header-brand">
+      Mitch Downey
+    </div>
+  </div><!-- /.header-nav -->
+  <div class="clearfix">
+  </div>
+</div><!-- /.col-sm-4 header-left -->
+<div class="col-sm-8 header-right">
+  <div class="header-social">
+    <div class="header-social-icon">
+      <i class="fa fa-github-square"></i>
+    </div>
+    <div class="header-social-icon">
+      <i class="fa fa-facebook-square"></i>
+    </div>
+    <div class="header-social-icon">
+      <i class="fa fa-twitter-square"></i>
+    </div>
+  </div><!-- /.header-social -->
+  <div class="header-nav-button">
+    Contact
+  </div>
+  <div class="header-nav-button">
+    Portfolio
+  </div>
+  <div class="clearfix">
+  </div>
+</div><!-- /.col-sm-8 header-right -->
+<div class="clearfix">
+</div>
---
+++ portfolio.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <title>mitchdowney.com</title>
+
+  <meta charset=”utf-8”> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+  <meta property="og:url" content="#" />
+  <meta property="og:title" content="#" />
+  <meta property="og:description" content="#" />
+  <meta property="og:image" content="#" />
+    
+  <link href="css/bootstrap.css" type="text/css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="font-awesome-4.0.3/css/font-awesome.min.css">
+  <link href="css/styles.css" type="text/css" rel="stylesheet">
+
+</head>
+  
+<body>
+
+<div class="container">
+
+  <?php include 'header.php'; ?>
+
+  <div class="col-sm-3 sidebar">
+      <div class="sidebar-title">
+        UX Designer
+      </div>
+      <div class="sidebar-title">
+        Web Developer
+      </div>
+      <br />
+      <div class="sidebar-title">
+        Chicago, IL
+      </div>
+      <br />
+      <div class="sidebar-title">
+        UX Skills
+      </div>
+      <div class="sidebar-text">
+        Axure, Adobe Fireworks, Illustrator, Photoshop, Wireframes, Prototypes, Competitive Analysis, Site Maps, Personas, Usability Testing, HCI, Responsive Design, Mobile Design
+      </div>
+      <br />
+      <div class="sidebar-title">
+        Programming Skills
+      </div>
+      <div class="sidebar-text">
+        HTML/CSS, Javascript, jQuery, Bootstrap, Python, Django, PHP, MySQL, Git
+      </div>
+      <br />
+      <div class="sidebar-title">
+        Education
+      </div>
+      <div class="sidebar-text">
+        Northern Illinois University
+      </div>
+      <div class="sidebar-subtext">
+        M.S.Ed. Instructional Technology
+      </div>
+      <div class="sidebar-text">
+        University of Illinois
+      </div>
+      <div class="sidebar-subtext">
+        BA English & Philosophy
+      </div>
+      <br />
+      <div class="sidebar-title">
+        Favorite Projects
+      </div>
+      <div class="sidebar-text">
+        Educational Software, Productivity Software, Social Networks, Free/Open Source
+      </div>
+  </div><!-- /.col-sm-3 side-bar -->
+
+  <div class="col-sm-9 main">
+    <div class="main-project-inner">
+      <div class="main-project-first color-everyvote">
+        <i class="fa fa-check-square-o indent-everyvote"></i> EveryVote
+      </div>
+      <div class="main-project color-partystarter">
+        <i class="fa fa-group indent-partystarter"></i> PartyStarter
+      </div>
+      <div class="main-project color-earthhq">
+        <i class="fa fa-globe indent-earthhq"></i> EarthHQ
+      </div>
+      <div class="main-project color-spotcheck">
+        <i class="fa fa-road indent-spotcheck"></i> SpotCheck
+      </div>
+      <div class="main-project color-advantage-act">
+        <i class="fa fa-book indent-advantage-act"></i> Advantage ACT
+      </div>
+      <div class="main-project color-top-video-timeline">
+        <i class="fa fa-film indent-advantage-act"></i> Top Video Timeline
+      </div>
+      <div class="clearfix">
+      </div>
+    </div><!-- /.main-project-inner -->
+  </div><!-- /.col-sm-9 main -->
+  <div class="clearfix">
+  </div>
+  <?php include 'footer.php'; ?>
+  
+</div><!-- /.container -->
+
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="js/bootstrap.js"></script>  
+
+</body>
