d0a8d847719b173544f4c01d619847ce4073210e
themitchuation <mitch@everyvote.org>
2014-02-17 17:42:16 -0600

    Change __unicode__ on Item in models.py

--- homepage/models.py
+++ homepage/models.py
@@ -43,7 +43,7 @@ class Item(models.Model):
     link_text = models.CharField(max_length=200)
     
     def __unicode__(self):
-        return u'%s - %s' % (self.name, self.project.name)
+        return u'%s - %s - %s' % (self.name, self.project.name, self.date)
         
     def get_absolute_url(self):
         return reverse('item_detail', kwargs={'pk': str(self.id)})
