7f5f86dbb5f839add89cad3548a8cc6d3c0517fa
themitchuation <mitch@everyvote.org>
2014-02-17 22:12:34 -0600

    Edit sorting / Add prototypes

--- homepage/models.py
+++ homepage/models.py
@@ -20,12 +20,18 @@ class Project(models.Model):
 class Tool(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ['name']
+
     def __unicode__(self):
         return u'%s' % (self.name)
 
 class Type(models.Model):
     name = models.CharField(max_length=100)
-    
+
+    class Meta:
+        ordering = ['name']
+
     def __unicode__(self):
         return u'%s' % (self.name)
         
